a7727abe667c5fd8f8a1d0b5336ba052d9553159
Taashu Sharma <taashusharma021@gmail.com>
2026-07-13 23:48:04 +0530

    AI backend

--- backend/routes/analyze.js
+++ backend/routes/analyze.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const analyzeController = require("../controller/analyzeController");
+const analyzeController = require("../controllers/analyzeController");
 
 router.post("/", analyzeController.analyze);
 router.post("/save", analyzeController.saveApplication);
